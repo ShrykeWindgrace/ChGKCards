523c5549e95f4c27684edb7997b3f125fb9330bf
Timofey Zakrevskiy <timofey.zakrevskiy@gmail.com>
2018-12-18 14:22:46 +0100

    :construction: refactor config
    need to rewrite:
    - parser
    - latex command representation
    
    need to introduce tui

--- cli/src/Cli.hs
+++ cli/src/Cli.hs
@@ -2,6 +2,7 @@ module Cli (CardConfig (..), defaultConfig, possibleConfigs, toFName) where
 
 import Data.List
 import Data.Char
+import Data.Maybe
 
 
 data CardConfig = CardConfig {
@@ -13,14 +14,69 @@ data CardConfig = CardConfig {
     printReserveTN :: Bool
 }
 
+data SubConf = SubConf {
+    countQ :: Int,
+    printT :: Bool
+}
+
+data Conf = Conf {
+    logo :: Bool,
+    printQ :: Bool,
+    tCount :: Int,
+    m :: SubConf,
+    s :: SubConf
+}
+
+convert :: Conf -> CardConfig
+convert (Conf _ printQ_ tCount_ (SubConf mqc mpt) (SubConf sqc spt)) = 
+    CardConfig tCount_ mqc (if sqc <= 0 then Nothing else Just sqc) mpt printQ_ spt
+
+
+convert' :: CardConfig -> Conf
+convert' (CardConfig tc qc res pt pq pr) = Conf
+    True
+    pq
+    tc
+    (SubConf qc pt)
+    (SubConf (fromMaybe 0 res) pr)
+
+
+
+defaultMain :: SubConf
+defaultMain = SubConf 36 True
+
+defaultSupp :: SubConf
+defaultSupp = SubConf 3 True
+
+deafultC :: Conf
+deafultC = Conf True True 1 defaultMain defaultSupp
+
+
+instance Show Conf where
+    show (Conf logo_ printQ_ tCount_ (SubConf mqc mpt) (SubConf sqc spt)) = 
+        intercalate "," $ [
+            "teams=" ++ show tCount_,
+            "questions=" ++ show mqc,
+            "printteamsatall=" ++ lower mpt,
+            "printquestions=" ++  lower printQ_,
+            "printreserveteamnumbers="++ lower spt,
+            "printlogo=" ++ lower logo_
+        ]
+        ++
+        case sqc of
+            0 -> ["printreserve=false"]
+            _ -> ["printreserve", "reserve=" ++ show sqc]
+
+
+
 instance Show CardConfig where
     show (CardConfig tc qc res pt pq pr) = 
         intercalate "," $ [
             "teams=" ++ show tc,
             "questions=" ++ show qc,
-            "printteamsatall=" ++ lower (show pt),
-            "printquestions=" ++  lower (show pq),
-            "printreserveteamnumbers="++  lower (show pr)
+            "printteamsatall=" ++ lower pt,
+            "printquestions=" ++  lower pq,
+            "printreserveteamnumbers="++  lower pr
         ]
         ++
         case res of
@@ -28,8 +84,8 @@ instance Show CardConfig where
             Just x -> ["printreserve", "reserve=" ++ show x]
 
 
-lower:: String -> String
-lower = fmap toLower
+lower:: Bool -> String
+lower b = fmap toLower (show b)
 
 defaultConfig :: CardConfig
 defaultConfig = CardConfig {
