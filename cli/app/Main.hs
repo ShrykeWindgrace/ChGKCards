ad30b6efb0ba3746d60446e064ed0e77907eb16c
Timofey Zakrevskiy <timofey.zakrevskiy@gmail.com>
2023-06-29 14:34:45 +0200

    Experiments

--- cli/app/Main.hs
+++ cli/app/Main.hs
@@ -2,18 +2,22 @@
 
 module Main where
 
-import Cli 
+import Cli
 import Options.Applicative (Parser, ParserInfo, help, long, showDefault,
                             flag,info, value,helper,
                              header, fullDesc, progDesc, execParser, auto, option)
 import           Control.Applicative (optional)
-import           System.Process.Typed 
+import           System.Process.Typed
 import           Data.ByteString.Lazy.Char8 (pack)
 import           System.Directory
 import           Control.Monad (unless)
+import           Text.Read
+import           Data.Char
 
 main :: IO ()
-main = execParser optionsH >>= main'
+--main = execParser optionsH >>= main'
+main = do
+    (CardConfig <$> getTeams <*> getQuests <*> getReserve <*> getPrintTeams <*> getPrintQuests <*> getPrintReserveNumbers) >>= print
 
 main' :: CardConfig -> IO ()
 main' cc = do
@@ -21,7 +25,7 @@ main' cc = do
     unless ex $ putStrLn $ "Warning: did not see style file " ++ styleFile ++ "; hope that pdflatex will find on itself"
     runProcess_ $ pipeConfig cc
 
- 
+
 styleFile :: FilePath
 styleFile = "chgkcard.cls"
 
@@ -35,7 +39,7 @@ pipeConfig cc = setStdin (byteStringInput $ pack $ texGenerator cc) pdfLaTeXproc
 
 
 texGenerator :: CardConfig -> String
-texGenerator cc = 
+texGenerator cc =
     "\\documentclass" ++
     "[" ++ show cc ++ "]" ++
     "{chgkcard}\\begin{document}\\end{document}"
@@ -94,3 +98,52 @@ options = CardConfig <$>
         help "print team numbers on reserve cards" <>
         showDefault
         )
+
+getTeams :: IO Int
+getTeams = do
+    putStrLn "teams? [default 6]"
+    resp <- getLine
+    case readMaybe resp of
+        Nothing -> pure 6
+        Just v -> pure $ if v > 0 then v else 6
+getQuests :: IO Int
+getQuests = do
+    putStrLn "questions? [default 36]"
+    resp <- getLine
+    case readMaybe resp of
+        Nothing -> pure 36
+        Just v -> pure $ if v > 0 then v else 36
+getReserve :: IO (Maybe Int)
+getReserve = do
+    putStrLn "reserve cards per team? [default 3]"
+    resp <- getLine
+    pure $ case readMaybe resp of
+        Nothing -> pure 3
+        Just v -> pure $ if v > 0 then v else 3
+
+getPrintTeams :: IO Bool
+getPrintTeams = do
+    putStrLn "printTeamNumbers? [Yn]"
+    resp <- getLine
+    pure $ case toLower <$> resp of
+        "" -> True
+        "y" -> True
+        _ -> False
+getPrintQuests :: IO Bool
+getPrintQuests = do
+    putStrLn "print question numbers? [Yn]"
+    resp <- getLine
+    pure $ case toLower <$> resp of
+        "" -> True
+        "y" -> True
+        _ -> False
+
+getPrintReserveNumbers :: IO Bool
+getPrintReserveNumbers = do
+    putStrLn "print team numbers on reserve casrds? [Yn]"
+    resp <- getLine
+    pure $ case toLower <$> resp of
+        "" -> True
+        "y" -> True
+        _ -> False
+
